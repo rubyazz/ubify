c868383bb3a5f1ef7b813b43b26dfd12f3ad4149
rubyazz <ersultan.abduvalov@gmail.com>
2023-04-15 18:38:02 +0600

    displaying admin

--- dash/admin.py
+++ dash/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from .models import Singer, Album, Song, Like
 
-admin.site.register(Singer)
-admin.site.register(Album)
-admin.site.register(Song)
-admin.site.register(Like)
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'nickname', 'img') # Specify fields to be displayed in the list view
+    list_filter = ('name',) # Add filters for filtering the list view
+    search_fields = ('name', 'nickname') # Add search fields for searching the list view
+    ordering = ('name',) # Specify the default sorting order
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'singer', 'img')
+    list_filter = ('singer',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'album', 'time', 'img')
+    list_filter = ('album',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'song')
+    list_filter = ('user',)
+    search_fields = ('user__username',)
+    ordering = ('user',)
