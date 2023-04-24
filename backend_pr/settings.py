175e488b0916ede2f615fd93c74271e357a042a8
rubyazz <ersultan.abduvalov@gmail.com>
2023-04-24 16:54:24 +0600

    template home

--- backend_pr/settings.py
+++ backend_pr/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
-from datetime import timedelta
 import os
+from datetime import timedelta
+from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -45,22 +46,19 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_yasg",
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.github',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.github",
 ]
 
 
 AUTHENTICATION_BACKENDS = [
-    
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
+    "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-    
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 
@@ -161,7 +159,7 @@ GRAPPELLI_SWITCH_USER = True
 
 
 AUTH_USER_MODEL = "users.CustomUser"
-ACCOUNT_EMAIL_REQUIRED = True 
+ACCOUNT_EMAIL_REQUIRED = True
 
 
 # DRF
@@ -187,14 +185,14 @@ SITE_ID = 1
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
+    "google": {
         # For each OAuth based provider, either add a ``SocialApp``
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
-        'APP': {
-            'client_id': '515207560160-3ptjp5ajcetel7h7v6ruoi9mn1i9amnq.apps.googleusercontent.com',
-            'secret': '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',
-            'key': ''
+        "APP": {
+            "client_id": "515207560160-3ptjp5ajcetel7h7v6ruoi9mn1i9amnq.apps.googleusercontent.com",
+            "secret": "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",
+            "key": "",
         }
     }
 }
